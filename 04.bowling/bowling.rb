66c409e313aaa2927aab9cbf368e61875c12e0e9
hongfu22 <jianye.hongfu@gmail.com>
2021-09-17 08:23:58 +0900

    指摘された箇所の修正完了

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -24,25 +24,23 @@ shots.each_slice(2) do |s|
   frames << s
 end
 
-# 最終ゲームが3球の場合の配列を作る
-# 最終でストライクが連続で出ると配列が増えすぎるので10組になるまで連結
-while frames.size > 10
-  tmp = frames.pop(2)
-  frames.push(tmp[0] + tmp[1])
-end
+# 最終ゲーム以降の投球を纏めて一次元配列に戻し格納
+frames[9] = frames[9..].flatten!
+# 纏めて代入した事でできた冗長部分を削除
+frames.slice!(10..)
 
 # 最終ゲームでのストライクにより9ゲーム目でのスペア
 # ストライクに影響が出ないようにストライク判定で入れた０を消去
 frames.last.each_with_index do |f, i|
-  frames.last.delete_at(i + 1) if f == 10 && frames.last[i + 1].zero?
+  frames.last.delete_at(i + 1) if f == 10
 end
 
 # 合計ポイント計算用
 point = 0
 
 # 連続ストライクの場合は2ゲーム先の1投目を足す為の関数
-# game_index:次ゲームの数字、args:各ゲーム各投球のスコアを入れた配列
-def strike_cal(game_index, game_array)
+# game_index:次ゲームの数字、game_array:各ゲーム各投球のスコアを入れた配列
+def calc_strike_score(game_index, game_array)
   if game_index < 9 && game_array[game_index][0] == 10
     game_array[game_index][0] + game_array[game_index + 1][0]
   else
@@ -60,7 +58,7 @@ frames.each_with_index do |frame, i|
 
   point +=
     if frame[0] == 10
-      10 + strike_cal(i + 1, frames)
+      10 + calc_strike_score(i + 1, frames)
     elsif frame.sum == 10
       10 + frames[i + 1][0]
     else
