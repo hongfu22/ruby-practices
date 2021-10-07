a21095e4197d4d3df1a48ee2f7c82604ba3c8785
hongfu22 <jianye.hongfu@gmail.com>
2021-10-07 23:25:17 +0900

    lsコマンド1回目バージョン3.0

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -10,26 +10,24 @@ file_path = argv.empty? ? './' : argv[0]
 
 class FileList
   TAB_LENGTH = 8
-
   def initialize(file_path, row)
-    @file_path = file_path
-    @row = row.to_f
+    @row = row
     # パス内の.始まりを除いたファイル、ディレクトリを取得
-    @files = Dir.glob('*', base: @file_path)
+    @files = Dir.glob('*', base: file_path)
   end
 
   # 起点メソッド
-  def main(row = @row)
+  def produce_file_lists(row = @row)
     return nil if @files.size.zero?
     # 列数の指定が1の場合はそのままファイルの配列を出力
-    return output_list(@files) if row.to_i == 1
+    return output_list(@files) if row == 1
 
     # 行数
-    col_num = (@files.size / row).ceil
+    col_num = (@files.size / row.to_f).ceil
     # ファイルパスの最長の長さを格納、後にこの長さにパスの長さを合わせる
     max_length = fetch_max_name_length(col_num, @files)
     # １行分を連結した幅がコマンドラインの幅よりも大きい場合は列数を少なくして再度関数呼び出し
-    return main(@row -= 1) if inspect_column_width?(max_length)
+    return produce_file_lists(@row -= 1) if inspect_column_width?(max_length)
 
     # 出力用に行ごとに文字列に纏め配列に格納する
     output_file_array = produce_each_column_array(col_num, max_length, @files)
@@ -44,11 +42,11 @@ class FileList
   end
 
   # 各列ごとに一番長い文字列の文字数を取得する
-  def fetch_max_name_length(col_num, file_array)
+  def fetch_max_name_length(col_num, files)
     max_length = 0
     # 指定されている列数ごとにパスの配列を順番に分け各列の最長文字数を取得
-    file_array.each_slice(col_num) do |files|
-      files.each do |file|
+    files.each_slice(col_num) do |files_in_a_row|
+      files_in_a_row.each do |file|
         # ファイル名の長さをはかる
         length = measure_name_width(file)
         max_length = length if max_length < length
@@ -94,7 +92,7 @@ end
 
 if Dir.exist?(file_path)
   file_list = FileList.new(file_path, 3)
-  file_list.main
+  file_list.produce_file_lists
 elsif File.exist?(file_path)
   puts file_path
 else
