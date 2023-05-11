b0569cc8dc37216fc613e1f1db530b0a1c29e9c3
hongfu22 <jianye.hongfu@gmail.com>
2023-05-12 08:20:31 +0900

    return系の記述の見直し、combined_target_contentの変数名変更

--- 08.ls_object/dir_content.rb
+++ 08.ls_object/dir_content.rb
@@ -4,6 +4,7 @@ require_relative './content_producer'
 
 class DirContent
   include ContentProducer
+
   TAB_LENGTH = 8
   private_constant :TAB_LENGTH
 
@@ -18,41 +19,48 @@ class DirContent
       puts format('ls.rb: %s: No such file or directory', @dir_path)
       return
     end
-    return output_dir_contents(@target_contents) if col_num == 1 || @target_contents.empty?
+
+    if col_num == 1 || @target_contents.empty?
+      output_dir_contents(@target_contents)
+      return
+    end
 
     row_num = (@target_contents.length / col_num.to_f).ceil
     max_length = cal_longest_file_name
-    return produce_dir_contents(@col_num -= 1) if too_long?(max_length)
+    if is_too_wide_row?(max_length)
+      produce_dir_contents(@col_num -= 1)
+      return
+    end
 
-    combined_target_contents = produce_each_row(row_num, max_length)
-    output_dir_contents(combined_target_contents)
+    rows = produce_each_row(row_num, max_length)
+    output_dir_contents(rows)
   end
 
   private
 
   def produce_each_row(row_num, max_length)
-    combined_target_contents = []
+    rows = []
     row_num.times do |row|
       each_row = []
       row.step(@target_contents.length, row_num) do |target_num|
         each_row << format("%-#{max_length}s", @target_contents[target_num]) unless @target_contents[target_num].nil?
       end
-      combined_target_contents << each_row.join("\t")
+      rows << each_row.join("\t")
     end
-    combined_target_contents
+    rows
   end
 
   def cal_longest_file_name
     @target_contents.map(&:length).max
   end
 
-  def too_long?(max_length)
+  def too_wide_row?(max_length)
     tab_width = TAB_LENGTH - max_length % TAB_LENGTH
     row_width = tab_width * (@col_num - 1) + max_length * @col_num
     row_width >= `tput cols`.to_i
   end
 
-  def output_dir_contents(combined_target_contents)
-    combined_target_contents.each { |combined_contents| puts combined_contents }
+  def output_dir_contents(rows)
+    rows.each { |row| puts row }
   end
 end
